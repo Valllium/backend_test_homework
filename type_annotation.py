a6b6c99f65ebd9428a68dd67f2e9b989e6468a1b
Alexander Dyuzhev <frduke@mail.ru>
2022-07-15 11:49:36 +0300

    test_work

---
+++ type_annotation.py
@@ -0,0 +1,19 @@
+from typing import Callable
+
+def add(number: float, callback: Callable[[float], float]) -> float:
+    """Производит арифметические действия с числами.
+    Принимает число и функцию, выполняющую арифметическое действие.
+    """    
+    return callback(number)
+
+
+def adder20(number):
+    """Добавляет к аргументу 20."""
+    return number + 20
+
+
+def multiplier2(number):
+    """Умножает аргумент на 2."""
+    return number * 2
+
+add(20,adder20(10))
